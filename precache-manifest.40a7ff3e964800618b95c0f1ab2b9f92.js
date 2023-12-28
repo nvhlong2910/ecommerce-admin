40f5ef28a019204029251da9f578990e91663bbc
nvhlong2910 <nvhlong2910@gmail.com>
2023-12-29 02:30:03 +0700

    Updates

---
+++ precache-manifest.40a7ff3e964800618b95c0f1ab2b9f92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1675e19daddbe4534dfe31ab5d0739f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83304a7c05ad450f74d5",
+    "url": "/static/css/2.c33d3e7c.chunk.css"
+  },
+  {
+    "revision": "17d01fb4abb9b24d7caa",
+    "url": "/static/css/main.a2aa1612.chunk.css"
+  },
+  {
+    "revision": "83304a7c05ad450f74d5",
+    "url": "/static/js/2.64c548e3.chunk.js"
+  },
+  {
+    "revision": "17d01fb4abb9b24d7caa",
+    "url": "/static/js/main.d019eb5b.chunk.js"
+  },
+  {
+    "revision": "2c94074b6f9a8fa452c0",
+    "url": "/static/js/runtime-main.4b9ced00.js"
+  }
+]);
