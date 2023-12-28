7c9916a6d3b4b9ed3fe45c6fa5ffafad3e3778d4
nvhlong2910 <nvhlong2910@gmail.com>
2023-12-29 02:22:27 +0700

    first commit

---
+++ src/config/index.js
@@ -0,0 +1,14 @@
+export const config = {
+  api: {
+    url: 'http://localhost:8080/api/v1'
+  },
+  firebaseConfig: {
+    apiKey: "AIzaSyBW8Sfxzd4ELyA041VMYS8p8yq1IXBC9uc",
+    authDomain: "ecommerce-c07e9.firebaseapp.com",
+    projectId: "ecommerce-c07e9",
+    storageBucket: "ecommerce-c07e9.appspot.com",
+    messagingSenderId: "451119235689",
+    appId: "1:451119235689:web:284918378a87ac30d7d8f8",
+    measurementId: "G-88ZBLPB8B0",
+  },
+}
